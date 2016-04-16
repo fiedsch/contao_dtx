1d2be1bf3af649ad9cccb3acd0b199870cfa5329
fiedsch <fiedsch@ja-eh.at>
2016-04-16 14:53:44 +0200

    create required subdirectories

--- make.sh
+++ make.sh
@@ -2,6 +2,15 @@
 
 # create the files
 
+for subdir in config dca elements languages/de templates
+do
+    if [ ! -d $subdir ]
+    then
+      echo "erzeuge Verzeichnis $subdir"
+      mkdir -p $subdir
+    fi
+done
+
 xetex demo.ins
 
 # create the documentation
